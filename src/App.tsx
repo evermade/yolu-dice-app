bef21fb6780156059f971d6febc75e92efa98d93
Antti Sartjärvi <antti.sartjarvi@evermade.fi>
2022-11-08 17:34:30 +0200

    Dice on, let's go

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { motion } from "framer-motion";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="decorators">
+        <div className="decorator"></div>
+        <div className="decorator"></div>
+        <div className="decorator"></div>
+      </div>
+      <motion.div
+        className="diceWrap"
+        whileHover={{ scale: 1.2 }}
+        whileTap={{ scale: 1.1 }}
+        drag="y"
+        dragConstraints={{ top: -10, bottom: 10 }}
+      >
+        <div className="dice rolling">
+          <div className="diceFace front"></div>
+          <div className="diceFace up"></div>
+          <div className="diceFace left"></div>
+          <div className="diceFace right"></div>
+          <div className="diceFace bottom"></div>
+          <div className="diceFace back"></div>
+        </div>
+      </motion.div>
     </div>
   );
 }
